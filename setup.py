7d6ffd0bf631c1a87891c9c85b7ee5bbbe0eb4c0
Ben Batorsky <yksrotab@gmail.com>
2018-01-26 12:01:34 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='model_helpers',
+      version='0.1',
+      description='Tools for helping construct/train models',
+      url='https://github.com/bpben/model_helpers',
+      author='BB',
+      author_email='yksrotab@gmail.com',
+      license='MIT',
+      packages=['model_classes'],
+      zip_safe=False)
