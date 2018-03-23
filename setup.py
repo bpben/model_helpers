9bc7e0eb880509280b1c56f491889e8416f90f81
Ben Batorsky <yksrotab@gmail.com>
2018-03-23 11:57:57 -0400

    prep for upload

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
 setup(name='model_helpers',
+      packages=['model_helpers'],
       version='0.1',
       description='Tools for helping construct/train models',
       url='https://github.com/bpben/model_helpers',
+      download_url='https://github.com/bpben/model_helpers/archive/0.1.tar.gz',
       author='BB',
       author_email='yksrotab@gmail.com',
-      license='MIT',
-      zip_safe=False)
+      keywords = ['models', 'ML', 'tuning', 'training'],
+      license='MIT')
